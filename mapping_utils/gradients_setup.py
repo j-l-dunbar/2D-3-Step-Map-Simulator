397158682260795543d98665a428ce25a35a71f6
James Dunbar <ja.l.dunbar@gmail.com>
2024-10-07 15:31:42 -0400

    working well -- can sim retinal injections easily -- map not converging very well with 400x400 grid

--- mapping_utils/gradients_setup.py
+++ mapping_utils/gradients_setup.py
@@ -10,7 +10,7 @@ start_time = datetime.now()
 
 
 
-Num = 50
+Num = 300
 retina = Tissue(Num)
 x = retina.grid_fract
 
@@ -90,64 +90,64 @@ df = make_map_df(np.random.permutation(Num**2), retina, colliculus)
 
 
 rc = Mapper(df, gamma=200, alpha=220, beta=220, R=0.11, d=0.03)
-refined_map = rc.refine_map(n_repeats=2E3)
+refined_map = rc.refine_map(n_repeats=6E4)
 
 print('Map Refined. Time Elapsed: {}'.format(datetime.now() - start_time))
 print(f'{refined_map.shape = }')
 
-#%%
-# RCmap = refined_map[5]
-
-# efnA_at_src = np.array([retina.efnA[*x] for x in retina.positions])
-# efnB_at_src = np.array([retina.efnB[*x] for x in retina.positions])
-# efnA_transp = efnA_at_src[RCmap.argsort()]
-# efnB_transp = efnB_at_src[RCmap.argsort()]
-
-# df2 = make_map_df(np.random.permutation(Num**2), retina, colliculus)
-
-# df2[6], df2[7] = efnA_transp[df2[5].astype(int)], efnB_transp[df2[5].astype(int)]
-
-# cc = Mapper(df2, gamma=200, alpha=120, beta=120, R=0.11, d=0.03)
 # #%%
-# refined_map = cc.refine_map()
+# # RCmap = refined_map[5]
+
+# # efnA_at_src = np.array([retina.efnA[*x] for x in retina.positions])
+# # efnB_at_src = np.array([retina.efnB[*x] for x in retina.positions])
+# # efnA_transp = efnA_at_src[RCmap.argsort()]
+# # efnB_transp = efnB_at_src[RCmap.argsort()]
+
+# # df2 = make_map_df(np.random.permutation(Num**2), retina, colliculus)
+
+# # df2[6], df2[7] = efnA_transp[df2[5].astype(int)], efnB_transp[df2[5].astype(int)]
+
+# # cc = Mapper(df2, gamma=200, alpha=120, beta=120, R=0.11, d=0.03)
+# # #%%
+# # refined_map = cc.refine_map()
 
 
-#%%
-fig, ax = plt.subplots(nrows=4, figsize=(5,20))
-ax[0].scatter(*refined_map[3:5], c=refined_map[9], cmap='turbo', s=4)
-ax[0].set_title('SCy')
-ax[1].scatter(*refined_map[3:5], c=refined_map[8], cmap='turbo', s=4)
-ax[1].set_title('SCx')
-ax[2].scatter(*refined_map[3:5], c=refined_map[7], cmap='turbo', s=4)
-ax[2].set_title('efnB')
-ax[3].scatter(*refined_map[3:5], c=refined_map[6], cmap='turbo', s=4)
-ax[3].set_title('efnA')
-#%%
-fig, ax = plt.subplots(nrows=4, figsize=(5,20))
-ax[0].scatter(*refined_map[8:], c=refined_map[1], cmap='turbo', s=4)
-ax[0].set_title('EphA')
-ax[1].scatter(*refined_map[8:], c=refined_map[2], cmap='turbo', s=4)
-ax[1].set_title('EphB')
-ax[2].scatter(*refined_map[8:], c=refined_map[3], cmap='turbo', s=4)
-ax[2].set_title('RetX')
-ax[3].scatter(*refined_map[8:], c=refined_map[4], cmap='turbo', s=4)
-ax[3].set_title('Rety')
-# %%
-
-# fig, ax = plt.subplots(nrows=2, figsize=(5,10))
-# ax[0].scatter(*refined_map[8:], c=refined_map[4], cmap='turbo_r', s=4)
-# ax[0].set_title('SC per Rety')
-# ax[1].scatter(*refined_map[3:5], c=refined_map[6], cmap='turbo', s=4)
-# ax[1].set_title('Ret per efnA')
-
-# # id_src, EphA_at_src, EphB_at_src, pos_at_src.T[0], pos_at_src.T[1], RCmap, efnA_at_trg, efnB_at_trg, pos_at_trg.T[0], pos_at_trg.T[1]
-# # 0,      1,           2,           3,               4,               5,     6,           7,           8,               9, 
+# #%%
+# fig, ax = plt.subplots(nrows=4, figsize=(5,20))
+# ax[0].scatter(*refined_map[3:5], c=refined_map[9], cmap='turbo', s=4)
+# ax[0].set_title('SCy')
+# ax[1].scatter(*refined_map[3:5], c=refined_map[8], cmap='turbo', s=4)
+# ax[1].set_title('SCx')
+# ax[2].scatter(*refined_map[3:5], c=refined_map[7], cmap='turbo', s=4)
+# ax[2].set_title('efnB')
+# ax[3].scatter(*refined_map[3:5], c=refined_map[6], cmap='turbo', s=4)
+# ax[3].set_title('efnA')
+# #%%
+# fig, ax = plt.subplots(nrows=4, figsize=(5,20))
+# ax[0].scatter(*refined_map[8:], c=refined_map[1], cmap='turbo', s=4)
+# ax[0].set_title('EphA')
+# ax[1].scatter(*refined_map[8:], c=refined_map[2], cmap='turbo', s=4)
+# ax[1].set_title('EphB')
+# ax[2].scatter(*refined_map[8:], c=refined_map[3], cmap='turbo', s=4)
+# ax[2].set_title('RetX')
+# ax[3].scatter(*refined_map[8:], c=refined_map[4], cmap='turbo', s=4)
+# ax[3].set_title('Rety')
 # # %%
-# injection_mask = np.logical_and(np.logical_and(refined_map[3]>0.40, refined_map[3]<0.50), np.logical_and(refined_map[4]>0.40, refined_map[4]<0.50))
-# fig, ax = plt.subplots(nrows=2, figsize=(5,10))
-# ax[0].scatter(*refined_map[3:5], c=injection_mask, cmap='turbo', s=7)
-# ax[1].scatter(*refined_map[8:], c=injection_mask, cmap='turbo', s=7)
-# ax[1].set_title('')
+
+# # fig, ax = plt.subplots(nrows=2, figsize=(5,10))
+# # ax[0].scatter(*refined_map[8:], c=refined_map[4], cmap='turbo_r', s=4)
+# # ax[0].set_title('SC per Rety')
+# # ax[1].scatter(*refined_map[3:5], c=refined_map[6], cmap='turbo', s=4)
+# # ax[1].set_title('Ret per efnA')
+
+# # # id_src, EphA_at_src, EphB_at_src, pos_at_src.T[0], pos_at_src.T[1], RCmap, efnA_at_trg, efnB_at_trg, pos_at_trg.T[0], pos_at_trg.T[1]
+# # # 0,      1,           2,           3,               4,               5,     6,           7,           8,               9, 
+# # # %%
+# # injection_mask = np.logical_and(np.logical_and(refined_map[3]>0.40, refined_map[3]<0.50), np.logical_and(refined_map[4]>0.40, refined_map[4]<0.50))
+# # fig, ax = plt.subplots(nrows=2, figsize=(5,10))
+# # ax[0].scatter(*refined_map[3:5], c=injection_mask, cmap='turbo', s=7)
+# # ax[1].scatter(*refined_map[8:], c=injection_mask, cmap='turbo', s=7)
+# # ax[1].set_title('')
 # # %%
 #%%
 def source_injection(df, inject=[0.5,0.5]):
@@ -183,7 +183,22 @@ def source_injection(df, inject=[0.5,0.5]):
     ax[1,1].imshow(trg_arr.T**0.001, cmap='turbo_r')
     fig.tight_layout()
     return fig
-#%%
+# %%
+
+fig = source_injection(refined_map, [0.5,0.5])
+
+
+end_time = datetime.now()
+print('Total Duration: {}'.format(end_time - start_time))
+
+
+# %%
+
+# fig1 = source_injection(refined_map, [0.2,0.22])
+
+# plt.show()
+# %%
+
 def source_injection2(df, inject=[0.5,0.5]):
     injection_arr = np.vstack((np.ones_like(df[3])*inject[0], np.ones_like(df[4])*inject[1])) # array representing point
     in_range_src = np.linalg.norm((df[3:5] - injection_arr), axis=0) # all distances to point (L2 Norm)
@@ -200,38 +215,31 @@ def source_injection2(df, inject=[0.5,0.5]):
     for c, i in zip(colliculus.positions, inj[hash_map]):
         trg_arr[*c] = i
         
-    return src_arr.T**0.001#, trg_arr.T**0.001
-
-fig = source_injection(refined_map, [0.5,0.5])
-
-
-end_time = datetime.now()
-print('Total Duration: {}'.format(end_time - start_time))
-
-
-# %%
-
-fig1 = source_injection(refined_map, [0.2,0.22])
-
-plt.show()
-# %%
+    return src_arr.T**0.001, trg_arr.T**0.001
 
 import matplotlib.cm as cm      
 from matplotlib.backend_bases import MouseButton
 
-fig = plt.imshow(source_injection2(refined_map, [0.5,0.5]), cm.gray_r)
+fig, ax = plt.subplots(ncols=2, figsize=(10,5))
 
+src, trg = source_injection2(refined_map, [0.5,0.5])
+ax[0].imshow(src, cmap='Greys')
+ax[1].imshow(trg, cmap='Greys')
 def onclick(event):
     if event.button is MouseButton.LEFT:
          inj = [event.xdata/Num, event.ydata/Num]
-    # clear frame
-    fig.set_data(source_injection2(refined_map, inj))
+    
+    src, trg = source_injection2(refined_map, inj)
+    ax[0].imshow(src, cmap='Greys')
+    ax[1].imshow(trg, cmap='Greys')
+    
     print(f'Clicked at: {inj}')
+    fig.canvas.draw()
 
 
 plt.axis('off')
-fig.axes.get_xaxis().set_visible(False)
-fig.axes.get_yaxis().set_visible(False)
+fig.axes[1].get_xaxis().set_visible(False)
+fig.axes[1].get_yaxis().set_visible(False)
 
 plt.connect('button_press_event',onclick)
 
